7fdfa595b940bba5afc62bc350dbe7df3a3927ce
Jon Toussaint <jondtoussaint@gmail.com>
2024-02-29 20:08:45 -0500

    implement step 1

---
+++ CloudCast/ForecastViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Jon Toussaint on 2/29/24.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
