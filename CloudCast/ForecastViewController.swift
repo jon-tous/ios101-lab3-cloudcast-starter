e7ad66407c23350907556b15ed9d536f443e80c9
Jon Toussaint <jondtoussaint@gmail.com>
2024-02-29 23:13:02 -0500

    implement step 4

--- CloudCast/ForecastViewController.swift
+++ CloudCast/ForecastViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ForecastViewController: UIViewController {
-
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var forecastImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
