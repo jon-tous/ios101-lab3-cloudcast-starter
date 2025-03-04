6b017d9232e6431e89d557c00e5333e1e22326f0
Jon Toussaint <jondtoussaint@gmail.com>
2025-03-03 20:24:49 -0500

    step 1

---
+++ CloudCast/ForecastViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Jon Toussaint on 3/3/25.
+//
+
+import UIKit
+
+// Class declaration, including the name of the class and its subclass (UIViewController)
+class ForecastViewController: UIViewController {
+
+    // Function override for the view controller
+    // This is fired when the view has finished loading for the first time
+    override func viewDidLoad() {
+        // Some functions require you to call the super class implementation
+        // Always read the online documentation to know if you need to
+        super.viewDidLoad()
+    }
+}
