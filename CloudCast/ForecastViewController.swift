bdc0ace8ed0a0b9ce50c98dab9e417fb0cf7019e
Jon Toussaint <jondtoussaint@gmail.com>
2025-06-20 18:42:23 -0600

    step 8

--- CloudCast/ForecastViewController.swift
+++ CloudCast/ForecastViewController.swift
@@ -36,7 +36,19 @@ class ForecastViewController: UIViewController {
         
         forecasts = createMockData()
         configure(with: forecasts[selectedForecastIndex])
+        
+        addGradient()
 
+    }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor(red: 0.54, green: 0.88, blue: 0.99, alpha: 1.00).cgColor,
+                                UIColor(red: 0.51, green: 0.81, blue: 0.97, alpha: 1.00).cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
     private func createMockData() -> [WeatherForecast] {
