49c734bbe26f1b5182624be7245b219cdfc3eaa9
Jon Toussaint <jondtoussaint@gmail.com>
2025-06-20 18:29:11 -0600

    step 4 and 5

--- CloudCast/ForecastViewController.swift
+++ CloudCast/ForecastViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ForecastViewController: UIViewController {
-
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var forecastImageView: UIImageView!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fakeData = WeatherForecast(temperature: 25.0, date: Date(), weatherCode: .partlyCloudy)
+        configure(with: fakeData)
 
+    }
+    
+    private func configure(with forecast: WeatherForecast) {
+        forecastImageView.image = forecast.weatherCode.image
+        descriptionLabel.text = forecast.weatherCode.description
+        temperatureLabel.text = "\(forecast.temperature)°F"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, yyyy"
+        dateLabel.text = dateFormatter.string(from: forecast.date)
     }
 
 }
